cdc9a58f8f04b90ca77fa62faf2594d46722ffd7
opaemerson <itequinti@gmail.com>
2023-10-04 17:52:57 -0300

    atualizao do filtro para retornar por categoria

--- filtrar.php
+++ filtrar.php
@@ -23,7 +23,27 @@ if (empty($procurarModelo)) {
     }else{
         echo json_encode(["message" => "Não possui marca cadastrada"]);
     }
+}
+
+if (empty($procurarModelo)) {
+  echo json_encode(["message" => "Não encontrado!"]);
+} else {
+  $sql = "SELECT a.*,
+  (SELECT u.nome FROM historico h INNER JOIN usuarios u ON u.id = h.usuario_id WHERE h.tag_id = a.id order by h.id DESC limit 1) as usuario
+   FROM heads a WHERE modelo LIKE '%$procurarModelo%'";
+  
+  $response = $conn->query($sql);
+
+  if($response && $response->num_rows > 0){ 
+
+      while($row = $response->fetch_assoc()){
+          $rows[] = $row;
+      }
+      echo json_encode($rows);
+  }else{
+      echo json_encode(["message" => "Não possui marca cadastrada"]);
   }
+}
   
   $conn->close();
 ?>
