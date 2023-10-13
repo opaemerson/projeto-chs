e1088f2e4fe4f3c387cdca8f698a6bd9d4a5b0cf
opaemerson <itequinti@gmai.com>
2023-10-13 11:40:58 -0300

    layout inicial

--- inicial.php
+++ inicial.php
@@ -6,16 +6,94 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
     <title>Projeto C.E.E</title>
+      <!-- Inclua a biblioteca Bootstrap (CSS e JS) aqui -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php
     session_start();
     ?>
 
-    <h1>Projeto C.E.E</h1>
-    <p><b>Objetivo:</b> fazer fkjsadogfh hgiuf fgjhdiu hfireu kpf.</p>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Empresa XYZ</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#projeto-cee">Projeto C.E.E</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#projeto-geekzero">Projeto Geekzero</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#projeto-dintask">Projeto Dintask</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#projeto-ganolia">Projeto Ganolia</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
+<div class="container mt-5" id="projeto-cee">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Projeto C.E.E</h1>
+                    <p class="card-text"><b>Objetivo:</b> Controle de Envio de Equipamentos.</p>
+                    <button type="button" onclick="verificarLogin()">Acessar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+<div class="container mt-5" id="projeto-geekzero">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Projeto Geekzero</h1>
+                    <p class="card-text"><b>Objetivo:</b> Notas sobre SÉRIES | FILMES | JOGOS VIRTUAIS | BOARDGAMES</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5" id="projeto-dintask">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Projeto Dintask</h1>
+                    <p class="card-text"><b>Objetivo:</b> Controle sobre qualquer tipo de tarefa</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5" id="projeto-ganolia">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Projeto Ganolia</h1>
+                    <p class="card-text"><b>Objetivo:</b> Boardgame único e híbrido</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
     <script>
         var usuarioLogado = <?php echo isset($_SESSION['nome']) ? 'true' : 'false'; ?>;
@@ -28,10 +106,6 @@
             }
         }
     </script>
-
-    <button type="button" onclick="verificarLogin()">Acessar</button>
-    
-
     
         <?php
         if (isset($_SESSION['nome'])) {
