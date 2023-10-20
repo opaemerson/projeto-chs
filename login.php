45a760aaf27dc5a2fe26b6f796518d964ad1f4ab
opaemerson <itequinti@gmai.com>
2023-10-20 20:17:19 -0300

    corrigindo config.php no login

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-require_once('../config.php');
+require_once('config.php');
 
 // Função para limpar e escapar dados do usuário
 function limparEntrada($entrada) {
