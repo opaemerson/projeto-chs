9f97bf11a193492f00596278283c60e279322595
opaemerson <itequinti@gmail.com>
2023-10-25 15:38:40 -0300

    detalhes do layout do mecanismo

--- mecanismo_ganolia/index_mecanismo.php
+++ mecanismo_ganolia/index_mecanismo.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>MecanismoG</title>
     <style>
         button {
@@ -27,10 +30,19 @@
         color: black;
         padding: 5px;
         }
+
+        body {font-family: "Lato", sans-serif}
+        .mySlides {display: none}
 </style>
 
 </head>
 <body>
+
+<div class="w3-bar w3-black w3-card">
+  <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+  <a href="../index.php" class="w3-bar-item w3-button w3-hover-red w3-padding-large custom-square">VOLTAR </a>
+  <a  class="w3-bar-item w3-button w3-padding-large custom-square" style="text-decoration: none;">GUIA</a>
+</div>
     <h2>Atacando</h2>
     <form action="processar_ataque.php" method="post">
     <label for="">Selecione o ID do Item Ofensivo:</label>
