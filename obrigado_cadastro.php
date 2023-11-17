b367ffefb4fffb535ec487e5a3c469d1070af19d
opaemerson <itequinti@gmail.com>
2023-11-17 15:21:01 -0300

    melhorando layout do obrigado_cadastro

--- obrigado_cadastro.php
+++ obrigado_cadastro.php
@@ -1,4 +1,28 @@
-Obrigado por cadastrar, espero que goste dos projetos.
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Seu Título Aqui</title>
+    <link rel="stylesheet" href="css/login.css">
+    <style>
+        body {
+            text-align: center; /* Centraliza o conteúdo na horizontal */
+            margin: 0; /* Remove as margens padrão do body */
+            padding: 0; /* Remove o preenchimento padrão do body */
+            height: 100vh; /* Faz com que o body ocupe a altura completa da viewport */
+            display: flex; /* Usa o modelo de layout flexível para centralizar verticalmente */
+            align-items: center; /* Centraliza o conteúdo na vertical */
+        }
 
-Faça seu login :D
-<a href="login.php">Acessar</a>
+    </style>
+</head>
+<body>
+    <div class="card">
+        <h1>Obrigado por cadastrar!</h1>
+        <p>Espero que goste dos projetos.</p>
+        <hr>
+        <a href="login.php">Acessar</a>
+    </div>
+</body>
+</html>
