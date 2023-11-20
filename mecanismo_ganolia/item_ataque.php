736ac53159d2f94715c563473f9b2df5eb55721c
opaemerson <itequinti@gmai.com>
2023-11-19 22:30:02 -0300

    correcao de bug e import de image

--- mecanismo_ganolia/item_ataque.php
+++ mecanismo_ganolia/item_ataque.php
@@ -29,15 +29,6 @@ if(isset($codigoItemAtaque) && $codigoItemAtaque !== ''){
             $damagePossivel = explode(";", $damage);
             $damageVisual = $damagePossivel[0] . " - " . $damagePossivel[count($damagePossivel) - 1];
         }
-
-        switch ($habilidade) {
-            case "Critico":
-                break;
-            case "Vampirismo":
-                break;
-            default:
-                break;
-        }
         
         $resposta['success'] = true;
         $resposta['nome'] = $nome;
@@ -61,28 +52,3 @@ if(isset($codigoItemAtaque) && $codigoItemAtaque !== ''){
 $conn->close();
 echo json_encode($resposta);
 ?>
-
-
-<div class="modal fade" id="modalColetivo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog">
-    <div class="modal-content">
-    <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Importar Arquivo</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    </div>
-    <div class="modal-body">
-        <form action="processar_upload.php" method="POST" enctype="multipart/form-data">
-        <input type="hidden" id="data_envio" class="form-control">
-        <input type="hidden" id="previsao" class="form-control">
-        <input type="hidden" id="retorno" class="form-control">
-        <input type="hidden" id="garantia" class="form-control">
-        <div class="mb-3">
-            <label for="arquivo">Selecione um arquivo:</label>
-            <input type="file" id="arquivo" name="arquivo">
-        </div>
-        <button type="submit" class="btn btn-primary" value="cadastrar">Enviar</button>
-        </form>
-    </div>
-    </div>
-</div>
-</div>
