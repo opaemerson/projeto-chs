509eae3f424d490af5c5f5bfa63fb143e9b2d38a
opaemerson <itequinti.gmail.com>
2024-01-06 17:13:01 -0300

    organizando pastas

--- projeto_chs/incluir_categoria.php
+++ projeto_chs/incluir_categoria.php
@@ -5,13 +5,22 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>CHS</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" href="../css/styles.css">
   <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@500&family=Roboto:wght@300&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Dosis:wght@500&family=Roboto:wght@300&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Dosis:wght@500&family=Oswald:wght@300&family=Playfair+Display:wght@500&family=Roboto:wght@300&display=swap" rel="stylesheet">
+<style>
+      .centralizar {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+    }
+</style>
 </head>
 <body>
 
+<div class="centralizar"> 
 <button type="button" class="btn-preto" data-bs-toggle="modal" data-bs-target="#addEquipamento">Adicionar Equipamento</button>
 <br>
 <br>
@@ -19,7 +28,7 @@
 <br>
 <br>
 <button type="button" class="btn-preto" data-bs-toggle="modal" data-bs-target="#addProblema">Adicionar Problema</button>
-
+</div>
 
 <div class="modal fade" id="addEquipamento" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -86,9 +95,9 @@
 <a href="index_chs.php" type="button" class="btn-preto">Voltar</a>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="../scripts/marca.js"></script>
-<script src="../scripts/problema.js"></script>
-<script src="../scripts/equipamento.js"></script>
+<script src="./scripts/marca.js"></script>
+<script src="./scripts/problema.js"></script>
+<script src="./scripts/equipamento.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
 </body>
