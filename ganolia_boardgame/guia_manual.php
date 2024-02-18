e3e8a644b4162b45bc6741738ccb5e55e59668ad
opaemerson <itequinti.gmail.com>
2024-02-18 20:29:00 -0300

    melhornado guia manual

--- ganolia_boardgame/guia_manual.php
+++ ganolia_boardgame/guia_manual.php
@@ -22,7 +22,7 @@
 <div class="two-columns">
     <p class="dropcap">
     Ganolia sera um Dungeon Crawler com um formato hibrido que combina elementos de boardgame com um aplicativo interativo, 
-    oferecendo aos jogadores a experiencia de explorar masmorras em um tabuleiro fisico, com miniaturas e dados, 
+    oferecendo aos jogadores a experiencia de explorar o vasto territorio de ganolia em um tabuleiro fisico, com miniaturas e dados, 
     enquanto gerenciam suas cartas virtuais para construir o deck perfeito.
     </p>
     <!--##topico##-->
@@ -50,20 +50,11 @@
 
     <!--##topico##-->
     <h3>Objetivo do jogo</h3>
-    
-    <h4>Quest Principal</h4>
-    <p>A ideia é criar uma inteligência que monte uma Quest personalizada para cada jogador, que pode combinar diferentes situações, como a fabricação de itens comuns e raros, a derrota de monstros e até mesmo a aniquilação de Chefes de Elite (Semi-Boss).</p>
-    
-    <h4>Quest Final</h4>
-    <p>Após todos os jogadores finalizarem as quests principais, terão a oportunidade de resolver a Quest do End Game. Um final com um Boss específico e aleatório para cada fim de jogo, equilibrando o poder do Boss de acordo com o número de jogadores. Todos os jogadores se juntam para tentar derrotar este Boss Final. </p>
-    <p>Ao derrotar o Boss Final, você ganhará um pacote, que oferecerá a oportunidade de adquirir itens para adicionar à sua biblioteca. Se você ou seu grupo conseguirem derrotar o Boss Final, a Quest do End Game será concluída, e o jogo será considerado "100% Concluído". Caso não consigam derrotar o Boss Final, a porcentagem de progresso no jogo será proporcional ao dano causado ao Boss, resultando em porcentagens variando de 90% a 99% de conclusão, indicando que o Boss foi derrotado parcialmente, mas não completamente.</p>
-    
+    <p>A ideia é destruir os portais que estão habilitados ao que chamamos de núcleos.</p>
+
     <!--##topico##-->
     <h2>Diferenciais do jogo</h2>
     <ul>
-    <li>
-    <p>Cada partida é propensa a ser única, utilizando inteligência para quests e garantindo a diversidade de experiência. Além disso, há a possibilidade de encantar itens durante as partidas atuais para uso em partidas futuras. </p>
-    </li>
 
     <li>
     <p>O jogo oferecerá uma venda de pacotes de miniaturas, ao comprar um pacote físico, você enriquecerá sua "biblioteca", Portanto agora, se o jogador decidir adentrar em uma partida o jogo será enriquecido com base nas cartas disponíveis em sua biblioteca, oferecendo oportunidades ao grupo de obtê-las também. </p>
@@ -74,24 +65,12 @@
     </li>
 
     <li>
-    <p>O jogo também irá proporcionará eventos exclusivos em um formato online, quando esta dungeon aparecer no mapa, os jogadores terão a oportunidade de se envolver.
-    No entanto, esse evento é realizado com um mapa exclusivo e dados virtuais, tudo no ambiente online. </p>
+    <p>Formato de encantar itens para agregar a sua biblioteca particular.</p>
     </li>
     </ul>
     
 
     <!--##topico##-->
-    <h2>Pacotes</h2>
-
-    <p>A ideia é dar início a uma comercialização fora do jogo padrão, desta maneira, busco encontrar uma história tão rica que faça união sobre a maioria dos personagens e monstros, deste modo é possível realizar a comercialização de miniaturas que estão fora do jogo padrão. </p>
-    Há dois tipos de pacotes disponíveis:
-
-    <h4>Pacote de Miniatura</h4> 
-    Este pacote inclui uma miniatura juntamente com algumas cartas aleatórias de equipamentos ou receitas.
-    
-    <h4>Pacote no Jogo</h4> 
-    Este pacote não contém a miniatura, mas pode ser adquirido por qualquer jogador durante a partida.
-
     <h2>O que seria a Biblioteca?</h2>
 
     <p>Este é o local de consulta na qual contém tanto as cartas de monstros quanto as de personagens que foram adquiridos. </p>
@@ -100,16 +79,8 @@
     
     <!--##topico##-->
     <h3>Mapas</h3>
-    <p>Os mapas sao as peças fisicas que se constroi ao centro da mesa...etc...</p>
-    
-    <h4>Territorio Central</h4>
-    <p>Essa parte do jogo é essencial, pois fornece um formato para posicionar as peças distintas. Geralmente, é nela que ocorre a centralização, reunindo todos os monstros dos territórios diferentes.</p>
-    
-    <h4>Territorio Distintos</h4>
-    <p>Essas são peças que se unem à parte central do mapa. Monstros de territórios distintos não têm a capacidade de invadir uns aos outros, mas podem coexistir no território central.</p>
-    
-    <h4>Objetos ??</h4>
-    <p>Adiconar eventos que introduzem objetos ao mapa, estes podem ser interagidos tanto com os personagens como os monstros.</p>
+    <h4>[em desenvolvimento...] </h4>
+    <p>Ideia: Adiconar eventos que introduzem objetos ao mapa, estes podem ser interagidos tanto com os personagens como os monstros.</p>
     
     <!--##topico##-->
 
@@ -118,8 +89,7 @@
     <p>Aquisição: Drop, Venda ou Conclusão de Qst.</p>
 
     <h3>Receitas</h3>
-    <p>Esta carta possui o poder de invocar um item, no entanto, é essencial fundir os itens mencionados na receita para criá-lo.</p>
-    <p>Aquisição: Drop, Compra ou Pacotes</p>
+    <p> São cartas que podem ser elaboradas ao surgir no centro de compras </p>
 
     <h3>Equipamentos</h3>
     <p>Os equipamentos estão divididos em várias categorias de raridade, e essa raridade pode influenciar os atributos do equipamento.</p>
@@ -143,13 +113,7 @@
     <p>As criaturas estão divididas em diferentes categorias de raridade, e essa raridade pode influenciar em seus atributos.</p>
     
     <h4>Recompensas de Criaturas</h4>
-    <p>A recompensa é concedida após a aniquilação de um monstro, ou seja, quando o HP do monstro é reduzido a zero. Existem dois tipos de recompensas: a recompensa por dano final e a recompensa por dano maior.</p>
-    <p>Isso implica que o jogador que infligir o maior dano ao monstro também receberá a recompensa correspondente. Se o jogador que causou o dano final for o mesmo que causou o dano maior, ele receberá duas recompensas.</p>
-    
-    <!--##topico##-->
-    <h3>Personagens</h3>
-    <p>Os personagens desempenham um papel crucial no jogo. Cada personagem é único, com um conjunto exclusivo de habilidades que os diferencia uns dos outros. Essas habilidades únicas podem ser vitais para o sucesso no jogo, pois oferecem vantagens estratégicas específicas.</p>
-    <p>Além disso, todos os personagens pertencem a uma classe, que define seu estilo de jogo e papel dentro da equipe. A classe de um personagem influencia suas habilidades e sua função em combate, proporcionando uma diversidade de estratégias e táticas para os jogadores explorarem.</p>
+    <p>A recompensa é concedida após a aniquilação de um monstro, ou seja, quando o HP do monstro é reduzido a zero. Existem dois tipos de recompensas: a recompensa por dano final e a recompensa por alvo</p>
 
     <!--##topico##-->
     <h3>Classe de Personagens</h3>
@@ -210,9 +174,6 @@
     
     <!--##topico##-->
     <h2>Ações</h2>
-    <p> Você possui três ações, sendo uma obrigatória e duas opcionais.</p>
-    
-    <h4>Ação Obrigatória</h4>
     <ul>
         <b>Manipulação:</b>
         Coletar 05 cartas de seu baralho de privilegios, destas cartas voce pode fazer as seguintes ações:
@@ -237,25 +198,19 @@
     <p><b>Comprar: </b>Moedas combinadas em sua mão, permitirá a compra de cartas em loja.</p>
 
     <h2>Combate</h2>
-    o cambate eh muito importante....(descricao)
-
+    
     <h4>Atacando</h4>
     <p>O jogador só pode realizar um ataque se estiver usando pelo menos um equipamento com poder de ataque e houver um monstro ao alcance. Se o jogador dispuser de três equipamentos, poderá executar até três ataques distintos.</p>
     <p>As cartas de ataque contêm uma marcação de dados que representa a probabilidade de sucesso do ataque. Ou seja, se o resultado do dado for igual ou maior que esse valor, o ataque é bem-sucedido.</p>
     <p>Além disso, alguns itens possuem habilidades que, ao serem ativadas durante um combate, aplicam um efeito ao alvo ou desencadeiam um evento relacionado à habilidade.</p>
     <p>Para que um ataque tenha sucesso, a taxa de acerto deve superar a taxa de defesa do inimigo. Suponhamos que você tenha três equipamentos à sua disposição; isso permite realizar até três ataques. Em cada ataque, você rola dados, variando de um D6 a um D12.</p>
-    <p>Cada equipamento possui sua própria taxa de acerto. Por exemplo, se uma espada tem uma taxa de acerto de 5 ou mais em um dado D6, isso significa que ela só causará dano se o resultado do dado for 5 ou superior. Além disso, o dano causado é indicado e calculado pelo aplicativo, e a faixa de dano que o equipamento pode infligir está descrita na carta correspondente.</p>
-    <p>É importante ressaltar que, além de rolar os dados de taxa de acerto, também é feita uma jogada dos dados de redução conforme indicado pela criatura. Dependendo do valor obtido nessa jogada, é possível reduzir o número resultante do dado de habilidade.</p>
+    <p>Cada criatura possui sua própria chance de acerto, denominado por CP. Isso significa que um jogador só causará dano se o resultado do dado for superior da criatura. Além disso, o dano causado é indicado e calculado pelo aplicativo, e a faixa de dano que o equipamento pode infligir está descrita na carta correspondente.</p>
 
     <h4>Situação Exemplo</h4>
-    <p>Para exemplificar, imagine três ataques, cada um usando um dado de seis lados, com sucesso ocorrendo quando o valor é igual ou superior a quatro. O primeiro ataque resulta em 4, o segundo em 2, e o terceiro em 5. O oponente lança um dado de seis lados como parte de sua defesa, e suponha que a carta de defesa do inimigo tenha reduzido a taxa de acerto em 1. Nesse cenário, apenas um ataque foi bem-sucedido.</p>
-    <p>Em seguida, o aplicativo calcula o dano de ataque de cada equipamento que obteve sucesso, variando de acordo com os equipamentos. Além disso, as cartas também fornecem informações sobre o dano máximo que um equipamento pode causar diretamente à vida do alvo.</p>
+    <p>[escrever...]</p>
 
     <h4>Defendendo</h4>
-    <p>A diferença entre ataque e defesa é a seguinte: ao se tratar da defesa, você escolhe o equipamento defensivo a ser utilizado na batalha, representando-o em todas as tentativas de defesa contra os ataques adversários. Em cada investida do oponente, o aplicativo automaticamente calcula se resultará em dano ou não, de maneira similar ao que ocorre com os ataques.</p>
-    <p>A resolução da batalha ocorre da seguinte forma, o item de defesa dará a probabilidad de remover valor de  No entanto, se a defesa não for suficiente, o ataque é realizado aleatoriamente, usando o poder de ataque indicado na carta do inimigo.</p>
-    <p>Quando um jogador derrota um monstro, ele recebe os benefícios associados à vitória. Além disso, quando um jogador enfrenta um monstro, ele também tem a oportunidade de receber recompensas.</p>
-
+    <p>[escrever...]</p>
 
 </div>
 </body>
