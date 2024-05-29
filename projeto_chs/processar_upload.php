15baa414e2c7062cd22285bf60bb2309455031ac
opaemerson <itequinti.gmail.com>
2024-05-28 22:40:41 -0300

    pq

--- projeto_chs/processar_upload.php
+++ projeto_chs/processar_upload.php
@@ -5,33 +5,33 @@ require_once('../config.php');
 if ($_SERVER["REQUEST_METHOD"] == "POST") { 
     $targetDirectory = "uploads/";
     $targetFile = $targetDirectory . basename($_FILES["arquivo"]["name"]);
-    $uploadOk = true;
     $csvFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-    // Verifica se o arquivo é do tipo CSV
-    if ($csvFileType != "csv") {
-        echo "Apenas arquivos CSV são permitidos.";
-        $uploadOk = false;
-    }
+    try{
 
-    if ($uploadOk) {
+        if ($csvFileType != "csv") {
+            throw new Exception('Formato nao permitido');
+        }
+
         if (move_uploaded_file($_FILES["arquivo"]["tmp_name"], $targetFile)) {
             $file = fopen($targetFile, "r");
             while (($line = fgetcsv($file)) !== FALSE) {
                 $valor = explode(";", $line[0]);
-
+    
                 $tag = $valor[0];
-                $modelo = $valor[1];
-                $problema = $valor[2];
-                $situacao = $valor[3];
-
-                $tagExistente = "SELECT id, data_envio, manutencao FROM chs_controle WHERE tag = '$tag'";
+                $equipamento = $valor[1];
+                $modelo = $valor[2];
+                $problema = $valor[3];
+                $situacao = $valor[4];
+    
+                $tagExistente = "SELECT id, data_envio, manutencao 
+                FROM chs_controle WHERE tag = '$tag'";
                 $resultado = $conn->query($tagExistente);
-
+    
                 if ($resultado->num_rows > 0){
                     
                     $row = $resultado->fetch_assoc();
-
+    
                     if ($situacao == 'Concluido'){
                         $data_envio = $row["data_envio"];
                         $data_previsao = 'Concluido';
@@ -42,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         $updateQuery = "UPDATE chs_controle SET modelo = '$modelo', problema = '$problema', situacao = '$situacao', previsao = '$data_previsao', retorno = '$data_retorno', garantia = '$garantia' WHERE tag = '$tag'";
                         $conn->query($updateQuery);
                         }
-
+    
                     if ($situacao == 'Enviado'){
                         $data_envio = date('d-m-Y');
                         $data_previsao = date('d-m-Y', strtotime($data_envio . '+7 days'));
@@ -53,17 +53,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         $updateQuery = "UPDATE chs_controle SET modelo = '".$modelo."', problema = '".$problema."', data_envio = '".$data_envio."', situacao = '".$situacao."', previsao = '".$data_previsao."', retorno = '".$data_retorno."', garantia = '".$data_garantia."', manutencao = '".$manutencao_novo."' WHERE tag = '".$tag."'";
                         $conn->query($updateQuery);
                     }
-
+    
                     }else{
-
+    
                         if (stripos($tag, 'TAG') !== false) {
                             continue;
                         }
-
+    
                         if (empty($tag)){
                             continue;
                         }
-
+    
                         if ($situacao == 'Enviado'){
                             $data_envio = date('d-m-Y');
                             $data_previsao = date('d-m-Y', strtotime($data_envio . '+7 days'));
@@ -78,7 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $data_retorno = '';
                             $garantia = '';
                         } 
-
+    
                         if ($situacao == 'Concluido'){
                             $data_envio = 'Nao obteve Envio';
                             $data_previsao = 'Concluido';
@@ -87,7 +87,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $diferenca_dias = floor($diferenca_dias / (60 * 60 * 24)); // converte para dias
                             $garantia = ($diferenca_dias > 30) ? 'Nao' : 'Sim';
                         }
-
+    
                         $sql = "INSERT INTO chs_controle (tag, modelo, problema, situacao, data_envio, previsao, retorno, garantia, manutencao) VALUES ('$tag', '$modelo', '$problema', '$situacao', '$data_envio' , '$data_previsao' , '$data_retorno', '$garantia', '$manutencao')";
                         $conn->query($sql) === TRUE;
                         }
@@ -96,11 +96,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         header("Location: index.php");
                         echo "Arquivo enviado e dados inseridos com sucesso.";
                         exit();
-        } else {
-            echo "Desculpe, ocorreu um erro ao enviar o arquivo." . "<br> Verifique se a planilha de importação não esteja aberta.";
         }
-    } else {
-        echo "Desculpe, o arquivo não foi enviado.";
+
+    }
+    catch (Exception $e) {
+        echo 'Exceção capturada: ',  $e->getMessage(), "\n";
     }
 }
 
