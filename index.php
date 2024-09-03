33a76a8e1286e956c5cc3ef0f046ce5161cef4aa
opaemerson <itequinti.gmail.com>
2024-09-03 19:14:32 -0300

    terceiro ajuste

--- index.php
+++ index.php
@@ -8,16 +8,6 @@
     <link rel="stylesheet" href="css/chs/cabecalho.css">   
     <link rel="stylesheet" href="css/chs/template1.css">
     <title>Projeto</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@500&family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Dosis:wght@500&family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Dosis:wght@500&family=Oswald:wght@300&family=Playfair+Display:wght@500&family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -31,7 +21,7 @@
                 <nav>
                     <?php
                     if (isset($_SESSION['nome'])) {
-                    echo '<a href="sair.php">SAIR</a>';
+                    echo '<a href="sair.php" class="botao-sair">SAIR</a>';
                     } else{
                     echo '<a href="login.php">ENTRAR</a>';
                     }
@@ -50,10 +40,10 @@
         </div>
     </header>
 <br><br>
-<p><b>Projeto:</b> Control Handling Sytem - CHS</p>
-<br><br>
-<button type="button" class="botao-carousel" onclick="verificarLogin('chs')">Acessar</button>
-
+<div class="centro">
+    <p><b>Projeto:</b> Control Handling Sytem - CHS</p>
+    <button type="button" class="botao-carousel" onclick="verificarLogin('chs')">Acessar</button>
+</div>
 
 <script>
         var usuarioLogado = <?php echo isset($_SESSION['nome']) ? 'true' : 'false'; ?>;
